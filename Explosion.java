14277430100efd03295285146f7ddfdade69b02a
AlexVesely <veselyalex11@gmail.com>
2024-12-04 18:02:22 +0000

    Create Explosion Class and image

--- Explosion.java
+++ Explosion.java
@@ -1,3 +1,20 @@
-public class Explosion {
+import javafx.scene.image.Image;
+
+public class Explosion extends Element {
+
+    public Explosion(int row, int column) {
+        super(row, column );
+        image = new Image("images/explosion.png");
+        canBeEntered = false;
+        canExplode = false;
+        name = "Explosion";
+    }
+
+
+
+    public static void createExplosion(int row, int column) {
+
+    }
+
 
 }
