09cbdea3c990d1e8b6db4fa4d6bb9933d6f16ef8
AlexVesely <veselyalex11@gmail.com>
2024-12-08 17:48:44 +0000

    Help with refactoring documenting amoebas and frog

--- Amoeba.java
+++ Amoeba.java
@@ -1,12 +1,16 @@
 import javafx.scene.image.Image;
 
-public class Amoeba extends Element {
 /**
- * Creates a new Amoeba object with the specified row and column.
- *
- * @param row the row of the Amoeba in the grid
- * @param column the column of the Amoeba in the grid
+ * This class represents an amoeba element on the game grid.
+ * @author Karamveer Singh
  */
+public class Amoeba extends Element {
+
+    /**
+     * Creates a new Amoeba object with the specified row and column.
+     * @param row the row of the Amoeba in the grid
+     * @param column the column of the Amoeba in the grid
+     */
     public Amoeba(final int row, final int column) {
         super(row, column);
         image = new Image("images/amoeba.png");
@@ -17,7 +21,6 @@ public class Amoeba extends Element {
 
     /**
      * Returns a string representation of the Amoeba object.
-     *
      * @return a string "Amoeba"
      */
     @Override
--- AmoebaManager.java
+++ AmoebaManager.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class manages all the Amoeba on the game's grid.
+ * @author Karamveer Singh
+ */
 public class AmoebaManager {
-    private static final List<AmoebaGroup> amoebaGroups = new ArrayList<>(); // Initialize properly
+    private static final List<AmoebaGroup> amoebaGroups = new ArrayList<>();
 
     /**
      * Returns true if there are no amoeba groups being managed, false otherwise.
-     *
      * @return true if there are no amoeba groups, false otherwise
      */
     public static boolean isEmpty() {
@@ -15,7 +18,6 @@ public class AmoebaManager {
 
     /**
      * Adds an AmoebaGroup to the list of managed amoeba groups.
-     *
      * @param group the AmoebaGroup to add
      */
     public static void addAmoebaGroup(AmoebaGroup group) {
@@ -34,11 +36,9 @@ public class AmoebaManager {
 
     /**
      * Updates all amoeba groups in the provided GridManager by spreading them.
-     *
      * @param gridManager the GridManager containing the amoeba groups to update
      */
     public static void updateAll(GridManager gridManager) {
-
         for (AmoebaGroup group : gridManager.getAmoebaGroups()) {
             group.spread(gridManager);
         }
